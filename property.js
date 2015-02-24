572d018cc044ea6acd0eee51c534b25c0d521244
KoryNunn <knunn187@gmail.com>
2015-02-24 17:11:41 +1000

    Removoved debug, updated enti

--- property.js
+++ property.js
@@ -13,10 +13,6 @@ module.exports = function createProperty(currentValue, changes){
             return binding && binding() || currentValue;
         }
 
-        if(typeof value === 'function'){
-            debugger;
-        }
-
         if(!Object.keys(previous.update(value)).length){
             return property;
         }
