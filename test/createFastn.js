a24b756469281863bf60f68bbde1110045ac17a4
KoryNunn <knunn187@gmail.com>
2015-06-10 17:53:09 +1000

    moved scheduled property updating to genericComponent

--- test/createFastn.js
+++ test/createFastn.js
@@ -1,4 +1,10 @@
 module.exports = function createFastn(){
+    var genericComponent = require('../genericComponent');
+
+    genericComponent.schedule = function(key, fn){
+        fn();
+    };
+
     return require('../')({
             _generic: require('../genericComponent'),
             list: require('../listComponent'),
