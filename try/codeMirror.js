c8ac08e36f90484911e7a347704fda67a350ce19
KoryNunn <knunn187@gmail.com>
2015-07-01 10:03:29 +1000

    Move codeMirror out to a seperate script tag load, massivly reduces minified app size. fixed some css.

--- try/codeMirror.js
+++ try/codeMirror.js
@@ -1,3 +1,1 @@
-// Singleton so the javascript mode only get set up once
-module.exports = CodeMirror = require('codemirror');
-require('codemirror/mode/javascript/javascript.js');
+module.exports = window.CodeMirror;
