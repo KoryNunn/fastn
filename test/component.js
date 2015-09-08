ac77bbdee47f735d53cbc6ee166dbfb9ef1ecc47
Kory Nunn <knunn187@gmail.com>
2015-09-08 14:42:53 +1000

    Added tests

--- test/component.js
+++ test/component.js
@@ -50,3 +50,85 @@ test('pre-created component', function(t){
 
     t.equal(component.scope().get('.'), data.foo);
 });
+
+test('auto extend component', function(t){
+
+    t.plan(6);
+
+    var fastn = createFastn({
+        foo: function(fastn, component, type, settings, children){
+            t.pass('Used foo constructor');
+            return component;
+        },
+        bar: function(fastn, component, type, settings, children){
+            t.pass('Used bar constructor');
+            return component;
+        },
+        baz: function(fastn, component, type, settings, children){
+            t.pass('Used baz constructor');
+            return component;
+        }
+    });
+
+    var component = fastn('foo:bar:baz');
+
+    t.ok(component.is('foo'), 'componant is foo');
+    t.ok(component.is('bar'), 'componant is bar');
+    t.ok(component.is('baz'), 'componant is baz');
+});
+
+test('manual extend component', function(t){
+
+    t.plan(6);
+
+    var fastn = createFastn({
+        foo: function(fastn, component, type, settings, children){
+            t.pass('Used foo constructor');
+            return component;
+        },
+        bar: function(fastn, component, type, settings, children){
+            t.pass('Used bar constructor');
+            return component;
+        },
+        baz: function(fastn, component, type, settings, children){
+            t.pass('Used baz constructor');
+            return component;
+        }
+    });
+
+    var component = fastn('foo');
+
+    component.extend('bar', {});
+
+    component.extend('baz', {});
+
+    t.ok(component.is('foo'), 'componant is foo');
+    t.ok(component.is('bar'), 'componant is bar');
+    t.ok(component.is('baz'), 'componant is baz');
+});
+
+test('cannot double-extend component', function(t){
+
+    t.plan(4);
+
+    var fastn = createFastn({
+        foo: function(fastn, component, type, settings, children){
+            t.pass('Used foo constructor');
+            return component;
+        },
+        bar: function(fastn, component, type, settings, children){
+            t.pass('Used bar constructor');
+            return component;
+        }
+    });
+
+    var component = fastn('foo');
+
+    component.extend('bar', {});
+
+    // Shouldn't cause another call to bar constructor.
+    component.extend('bar', {});
+
+    t.ok(component.is('foo'), 'componant is foo');
+    t.ok(component.is('bar'), 'componant is bar');
+});
